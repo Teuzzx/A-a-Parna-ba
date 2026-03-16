cec813caa995ec2c0649e3910be8a8c116ca0e0a
Mateus Victor <132577315+Teuzzx@users.noreply.github.com>
2026-03-16 01:02:25 -0300

    Add files via upload

---
+++ js/dados.js
@@ -0,0 +1,99 @@
+var MENU = {
+    "burgers": [
+        {
+            "id": "the-gramercy-tavern-burger-4-pack",
+            "img": "./img/cardapio/burguers/Gramercy-Tavern-Burger-and-Kielbasa-Kit-6.4.21-72ppi-1x1-15.jpg",
+            "name": "Simples Bob",
+            "dsc": "The Gramercy Tavern Burger - 4 Pack",
+            "price": 12
+        },
+        {
+            "id": "shake-shack-shackburger-8-pack",
+            "img": "./img/cardapio/burguers/shake-shack-shackburger-8-pack.973a5e26836ea86d7e86a327becea2b0.jpg",
+            "name": "X-Burguer Bob",
+            "dsc": "Shake Shack ShackBurger® - 8 Pack",
+            "price": 14
+        },
+        {
+            "id": "gotts-cheeseburger-kit-for-4",
+            "img": "./img/cardapio/burguers/gotts-complete-cheeseburger-kit-for-4.7bdc74104b193427b3fe6eae39e05b5e.jpg",
+            "name": "X-Salada Bob",
+            "dsc": "Gott's Complete Cheeseburger Kit for 4",
+            "price": 15
+        },
+        {
+            "id": "le-big-matt-kit-for-6",
+            "img": "./img/cardapio/burguers/le-big-matt-kit-for-6.1ddae6e382bb3218eeb0fd5247de115a.jpg",
+            "name": "X-Bacon Bob",
+            "dsc": "Le Big Matt Burger Kit for 6",
+            "price": 17
+        },
+        {
+            "id": "shake-shack-shackburger-16-pack",
+            "img": "./img/cardapio/burguers/shake-shack-shackburger-16-pack.316f8b09144db65931ea29e34869287a.jpg",
+            "name": "X-Calabresa Bob",
+            "dsc": "Shake Shack Shackburger® - 16 Pack",
+            "price": 17
+        },
+        {
+            "id": "21-usda-prime-burgers-pack-of-18-8oz-each",
+            "img": "./img/cardapio/burguers/usda-prime-burgers-pack-of-18-8oz-each.274c67f15aa1c0b210dbf51801706670.jpg",
+            "name": "X-Triplo Bob",
+            "dsc": "USDA Prime Burgers - Pack of 18 (8oz each)",
+            "price": 25
+        },
+    ],
+    "pizzas": [
+        {
+            "id": "2-lou-malnatis-deep-dish-pizzas",
+            "img": "img/Batata-frita.jpg",
+            "name": "Batata Frita 150g",
+            "dsc": "2 Lou Malnati's Deep Dish Pizzas",
+            "price": 9
+        },
+        {
+            "id": "23699-choose-your-own-thin-crust-pizza-4-pack",
+            "img": "img/Batata-frita.jpg",
+            "name": "Batata Frita 300g",
+            "dsc": "Choose Your Own Thin Crust Pizza - 4 Pack",
+            "price": 13
+        },
+        {
+            "id": "choose-your-own-new-haven-style-pizza-6-pack",
+            "img": "img/batata-frita-cheddar-bacon.jpg",
+            "name": "Batata com Cheddar e Bacon 150g",
+            "dsc": "New Haven-Style Pizza - 6 Pack (Choose Your Own)",
+            "price": 18
+        },
+        {
+            "id": "6-lou-malnatis-deep-dish-pizzas",
+            "img": "img/batata-frita-cheddar-bacon.jpg",
+            "name": "Batata com Cheddar e Bacon 300g",
+            "dsc": "6 Lou Malnati's Deep Dish Pizzas",
+            "price": 25
+        },
+    ],
+    "steaks": [
+        {
+            "id": "california-reserve-filet-mignon-steaks-gift-box",
+            "img": "img/Refri de 1L (1).png",
+            "name": "Coca, Fanta e Guaraná 1L",
+            "dsc": "California Reserve Filet Mignon Steaks Gift Box",
+            "price": 9
+        },
+        {
+            "id": "steaks-and-cakes-date-night-dinner-for-2",
+            "img": "img/Refri de 1L (1).png",
+            "name": "Coca, Fanta e Guaraná 1,5L",
+            "dsc": "Steaks and Cakes Date Night Dinner for 2",
+            "price": 12
+        },
+        {
+            "id": "Prime-holiday-steak-sampler-for-10-12",
+            "img": "img/Refri lata.png",
+            "name": "Coca, Fanta e Guaraná 350ML",
+            "dsc": "Chef Matt's Steak Sampler for 10-12",
+            "price": 6
+        },
+    ]
+}
