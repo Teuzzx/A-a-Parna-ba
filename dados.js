5523272611c325f9be77e37b32f395d6bb25898e
Mateus Victor <132577315+Teuzzx@users.noreply.github.com>
2026-03-16 11:39:12 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,62 @@
+var MENU = {
+    "acai": [
+        {
+            "id": "acai-p",
+            "img": "img/açaí-tigela.png",
+            "name": "Açaí - Tamanho P",
+            "dsc": "P (Pequeno)",
+            "price": 12.00
+        },
+        {
+            "id": "acai-m",
+            "img": "img/açaí-tigela.png",
+            "name": "Açaí - Tamanho M",
+            "dsc": "M (Médio)",
+            "price": 15.00
+        },
+        {
+            "id": "acai-g",
+            "img": "img/açaí-tigela.png",
+            "name": "Açaí - Tamanho G",
+            "dsc": "G (Grande)",
+            "price": 18.00
+        },
+        {
+            "id": "acai-gg",
+            "img": "img/açaí-tigela.png",
+            "name": "Açaí - Tamanho GG",
+            "dsc": "GG",
+            "price": 22.00
+        },
+        {
+            "id": "acai-extrag",
+            "img": "img/açaí-tigela.png",
+            "name": "Açaí - Tamanho Extra G",
+            "dsc": "Extra G",
+            "price": 25.00
+        }
+    ]
+}
+
+var INGREDIENTES = {
+    "coberturas": [
+        { "id": "leite-condensado", "name": "Leite condensado" },
+        { "id": "calda-chocolate", "name": "Calda de chocolate" },
+        { "id": "calda-morango", "name": "Calda de morango" },
+        { "id": "mel", "name": "Mel" }
+    ],
+    "complementos": [
+        { "id": "leite-po", "name": "Leite em pó" },
+        { "id": "farinha-lactea", "name": "Farinha láctea" },
+        { "id": "granola", "name": "Granola" },
+        { "id": "cereal", "name": "Cereal" },
+        { "id": "pacoca", "name": "Paçoca" },
+        { "id": "amendoim", "name": "Amendoim" },
+        { "id": "castanha", "name": "Castanha" },
+        { "id": "coco-ralado", "name": "Coco ralado" },
+        { "id": "banana", "name": "Banana" },
+        { "id": "morango", "name": "Morango" },
+        { "id": "m-ms", "name": "M&M's" },
+        { "id": "gotas-chocolate", "name": "Gotas de chocolate" }
+    ]
+}
